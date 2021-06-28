62971fe25f4f846cc46979a3f38e85183ca4182a
adotahirovich <80359702+adotahirovich@users.noreply.github.com>
2021-06-28 23:43:01 +0200

    update config.php

--- ePharmacy-main/api/config.php
+++ ePharmacy-main/api/config.php
@@ -27,13 +27,8 @@ class Config
         return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
     }
     
-    //ovde mozes koristiti ili mailgun kao ja, ili gmail samo ne znam kako ide gmail, to ima lab snimljen za to
     const SMTP_HOST = "smtp.mailgun.org";
     const SMTP_PORT = 587;
-
-    //Ovo dvoje ispod moras kreirati account na mailgun ili koristeci svoj gmail, mailgun znam kako ide, gmail 
-    //ne znam njega je keco koristio, tako da odaberi pa samo kad registrujes acc ubaci dole info
-    
     const SMTP_USER = "";
     const SMTP_PASSWORD = "";
 }
